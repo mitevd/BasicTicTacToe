5bf5ef098cf085b720911bea892caae7d93cecbb
Dimitar Mitev <96146262+mitevd@users.noreply.github.com>
2022-03-14 23:32:37 +0200

    Add files via upload

---
+++ wild_farm/birds.py
@@ -0,0 +1,29 @@
+from wild_farm.project.animals.animal import Bird
+
+
+class Owl(Bird):
+    ALLOWED_FOODS = ['Meat']
+    WEIGHT_INCREMENTAL = 0.25
+
+    def __init__(self, name, weight, wing_size):
+        super().__init__(name, weight, wing_size)
+
+    def make_sound(self):
+        return 'Hoot Hoot'
+
+
+class Hen(Bird):
+    ALLOWED_FOODS = ['Meat', 'Vegetable', 'Fruit', 'Seed']
+    WEIGHT_INCREMENTAL = 0.35
+
+    def __init__(self, name, weight, wing_size):
+        super().__init__(name, weight, wing_size)
+
+    def make_sound(self):
+        return 'Cluck'
+
+
+
+
+
+
